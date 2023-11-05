9b63add9216edc47d58c3f8ab858d34f326879af
ju-eberg <j.ehrenberg@campus.lmu.de>
2023-11-05 19:37:40 +0100

    Nachholung Blatt 2

--- Uebungen.R
+++ Uebungen.R
@@ -97,5 +97,122 @@ for (p in Werte_p) {
 
 ####### Ende 1.3
 
-####
+######## BLATT 2
 
+#2.1 und 2.2 nur in Lösung auf GoodNotest
+
+##2.3:
+
+#' @param X Matrix mit Zeilen X_1, ..., X_n.
+#' @param beta Parametervektor
+
+sigma_2 <- function(X, beta) {
+  n <- nrow(X)
+  d <- ncol(X)
+  Sig <- matrix(0, d, d)
+  for (i in 1:n) {
+    Sig <- Sig + (X[i, ] - colMeans(X)) %*% t(X[i, ] - colMeans(X))
+  }
+  as.numeric(t(beta) %*% Sig %*% beta / n)
+}
+
+# 
+# (a) Gebe die Laufzeit- und Speicher-Komplexität des Algorithmus in Landau-Notation an.
+# 
+# Lösung
+# 
+# Speicherplatz für X: O(nd)
+# 
+# Speicherplatz für beta: O(d)
+# 
+# Speicherplatz für Sig: O(d ^2)
+# 
+# Insgesamt haben wir also O(nd + d^2)
+# 
+# 
+# Laufzeit für colMeans(X): O(nd + d) = O(nd) (n − 1 Additionen für jede der d Spalten
+#                                              und d Multiplikationen für die Durchschnittsbildung)
+# 
+#  
+# Laufzeit für (X[i, ] - colMeans(X)) %*% t(X[i, ] - colMeans(X)): O(nd + d + d^2) = O(nd + d^2)
+#  (O(nd) Operationen für colMeans(X), 2d Subtraktionen, d^2 Multiplikationen für das äußere Produkt$)
+# 
+# • n Iterationen über i.
+# 
+# Insgesamt haben wir also O(n(nd + d^2)) = =(nd^2 + n^2d)
+# 
+
+#  # b)  
+# Überprüfe die Laufzeiteigenschaften numerisch. Erzeuge dazu mehrere Datensätze
+# mit n ∈ [10, 1000] und d ∈ [10, 1000] und benchmarke den Algorithmus (z.B. mit
+#                                         microbenchmark::microbenchmark())
+
+library(microbenchmark)
+library(ggplot2)
+
+grid <- expand.grid(
+  n = seq.int(10, 1000, length = 4),
+  d = seq.int(10, 1000, length = 4)
+)
+
+times <- numeric(nrow(grid))
+
+for (k in 1:nrow(grid)) {
+  n <- grid[k, 1]
+  d <- grid[k, 2]
+  X <- replicate(d, rnorm(n))
+  cat("n = ", n, ", d = ", d, ", time = ", sep = "")
+  times[k] <- median(microbenchmark(sigma_2(X, rep(1, d)), times = 10)$time)
+  cat(times[k] / 1e9, "\n")
+}
+
+cbind(grid, time = times) |>
+  ggplot(aes(n, time, color = as.factor(d))) +
+  geom_line()
+
+cbind(grid, time = times) |>
+  ggplot(aes(d, time, color = as.factor(n))) +
+  geom_line()
+
+
+#######
+# (c) Finde und benchmarke einen alternativen Algorithmus mit Laufzeit- und Speicherkomplexität O(nd)
+
+# Kernideen in Lsg nachlesen
+
+#' @param X Matrix mit Zeilen X_1, ..., X_n.
+#' @param beta Parametervektor
+
+#' @param X Matrix mit Zeilen X_1, ..., X_n.
+#' @param beta Parametervektor
+sigma_2_besser <- function(X, beta) {
+  Xbeta <- X %*% beta
+  mu <- mean(Xbeta)
+  mean((Xbeta - mu)^2)
+}
+
+grid <- expand.grid(
+  n = seq.int(10, 1000, length = 4),
+  d = seq.int(10, 1000, length = 4)
+)
+
+times <- numeric(nrow(grid))
+
+for (k in 1:nrow(grid)) {
+  n <- grid[k, 1]
+  d <- grid[k, 2]
+  X <- replicate(d, rnorm(n))
+  cat("n = ", n, ", d = ", d, ", time = ", sep = "")
+  times[k] <- median(microbenchmark(sigma_2_besser(X, rep(1, d)), times = 1000)$time)
+  cat(times[k] / 1e9, "\n")
+}
+
+cbind(grid, time = times) |>
+  ggplot(aes(n, time, color = as.factor(d))) +
+  geom_line()
+
+cbind(grid, time = times) |>
+  ggplot(aes(d, time, color = as.factor(n))) +
+  geom_line()
+
+# Wir sehen also, dass die Laufzeit von d und n hier tatsächlich linear sind.
